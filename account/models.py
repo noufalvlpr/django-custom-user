6310bc1cc6705078ab635910cd9958063f47a0de
Noufal Valapra <noufal.valapra@fingent.com>
2018-09-14 15:38:46 +0530

    usermanager create user

--- account/models.py
+++ account/models.py
@@ -21,10 +21,8 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, email, password, **extra_fields)
+    def create_user(self, email=None, password=None, **extra_fields):
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('user_type', 'ADMIN')
